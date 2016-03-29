fb2fc79a4256d0c90cff90b4e357c5d45eb1ece3
Michael Green <mgreen@pc-mac-mgreen.local>
2016-03-29 13:19:21 -0500

    added partials and sticky header

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,14 +1,17 @@
 $(document).foundation();
 
+
+var headerHeight = $("div.top-bar").height();
+var customoffset = 25
+
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
+      var scrollToPosition = $(target).offset().top - headerHeight;
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
+        $('html, body').animate({ 'scrollTop': scrollToPosition - customoffset}, 1000);
         return false;
       }
     }
